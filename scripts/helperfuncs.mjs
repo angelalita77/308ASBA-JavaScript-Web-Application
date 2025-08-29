e073d51a4e944775d532a3a7f04cf6cb4ad44e72
angelalita77 <anjeanette@gmail.com>
2025-08-28 22:06:55 -0400

    Added some comments

--- scripts/helperfuncs.mjs
+++ scripts/helperfuncs.mjs
@@ -1,2 +1,34 @@
-//create a function to ensure the quote are not explicit
+ const forbidden = ["panties","boob","boobs", "ass", "panty"];
 
+// Create a function to ensure the quote displayed is not explicit.
+// If the quote is explicit, then the function will be called again
+// to pull another random quote
+// Argument: data[0].quote which stores the quote object value (string) 
+// Return: Boolean
+export function safeQuote(quote){
+    // test variable with array of regular and forbidden words  
+    const lowercaseQuote = quote.toLowerCase()
+    const quoteArray = lowercaseQuote.split(` `);
+    console.log(lowercaseQuote);
+    console.log(quoteArray);
+
+    for (let word of quoteArray){
+        console.log(word)
+        for(let i = 0; i < forbidden.length; i++){
+            console.log(forbidden[i])
+            if (forbidden[i] !== word){
+                continue;
+            }
+            else{
+                 console.log("Not Allowed: " + quote)
+                 console.log("Forbidden word: " + word)
+                 return false;
+            }
+
+        }
+
+    }
+    // After checking each word in the string
+    // return TRUE if no forbidden words were found 
+    return true;
+}
--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,4 +1,6 @@
 import { randomUrl, charUrl} from "./urls.mjs";
+import { safeQuote } from "./helperfuncs.mjs";
+
 const p = document.getElementById('quote');
 const speaker = document.getElementById('speaker');
 const show = document.getElementById('show')
@@ -6,6 +8,8 @@ const quote_btn = document.getElementById('quote_btn')
 
 
 quote_btn.addEventListener('click', fetchData)
+
+// For testing API fetches
 // fetch(url)
 //     .then(response => response.json())
 //     .then(quote => console.log(quote))
@@ -19,21 +23,20 @@ async function fetchData() {
         if (!response.ok) {
             throw new Error(`Bruh! \nHTTP error! status: ${response.status}`)
         }
-        // fetch data and convert to obj array
+        // fetch data and convert to json obj array
         const data = await response.json();
+
+        //Check to see if quote has no explicit words
+        //returns TRUE if quote is clean
         if (safeQuote(data[0].quote)) {
- 
-        
-        
-        //fetchImage(data);
         // Since it's random, fetch the quote from the first obj in array
         show.innerHTML = data[0].show; // anime title
         p.innerHTML = `"` + data[0].quote + `"`; // quote
         speaker.innerHTML = "-" + data[0].character; //quote speaker
-        
-        console.log(`Data received:`, data[0].quote)
         }
         else {
+            // If a forbidden word is found, call function again
+            // to fetch another random quote.
             fetchData();
         }
         
@@ -46,50 +49,6 @@ async function fetchData() {
 
 
 //------------------------- Ideas of Helper Functions to be Imported ----------------------/
-
-
-// Create a function to ensure the quote displayed is not explicit.
-// If the quote is explicit, then the function will be called again
-// to pull another random quote
-// Argument: data[0].quote which stores the quote object value (string) 
-// Return: Boolean
-function safeQuote(quote){
-    // test variable with array of regular and forbidden words 
-    const forbidden = ["house","cat","will", "ass", "your"];
-    const lowercaseQuote = quote.toLowerCase()
-    const quoteArray = lowercaseQuote.split(` `);
-    console.log(lowercaseQuote);
-    console.log(quoteArray);
-
-    for (let word of quoteArray){
-        console.log(word)
-        for(let i = 0; i < forbidden.length; i++){
-            console.log(forbidden[i])
-            if (forbidden[i] !== word){
-                continue;
-            }
-            else{
-                 console.log("Not Allowed: " + quote)
-                 console.log("Forbidden word: " + word)
-                 return false;
-            }
-
-        }
-
-    }
-    /* retrive string of quote
-    check string for explicit words
-    return TRUE if safe
-    else call fetchData*/
-    return true;
-}
-
-
-
-
-
-
-
 // // Retrieve image of the speaker of the quote 
 // // Get image from API stored in charURL imported from ./urls.mjs
 // // Testing before placing in module file ./helperfuncs.mjs
